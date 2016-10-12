d39e0b4014e15bd5e376efb4776802f5f8189f28
Robert M. Parker <rparker@yamiko.ninja>
2016-10-12 03:29:27 -0700

    fancybox adjustments

--- wp-content/themes/aphmau/web/scripts/app.js
+++ wp-content/themes/aphmau/web/scripts/app.js
@@ -1,10 +1,7 @@
 jQuery(function() {
     jQuery(document).foundation();
-    jQuery('.fancybox').fancybox();
-    jQuery('.fancybox-media').fancybox({
-        openEffect  : 'none',
-        closeEffect : 'none',
-        helpers : {
+    jQuery('.fancybox').fancybox({
+        helpers: {
             media : {},
             overlay: {
                 locked: false
